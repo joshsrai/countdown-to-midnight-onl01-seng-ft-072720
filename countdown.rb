1150856aef3de4c6532484bdaace5e3a37479157
Josh Raiborde <joshsrai@yahoo.com>
2020-07-28 00:31:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,4 +8,11 @@ end
 'HAPPY NEW YEAR!'
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(number)
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  number -= 1 
+  sleep(1)
+end
+'HAPPY NEW YEAR!'
+end
